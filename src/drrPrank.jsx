075a623f3a397c94660bd1ecb3ceda0b04c67715
Yosef <yosefplayz4580@gmail.com>
2023-03-16 18:39:38 +0200

    fixed smth rushuby needed

--- src/drrPrank.jsx
+++ src/drrPrank.jsx
@@ -1,18 +1,30 @@
 import DRRbg from './assets/homePage.png'
 import DRRgate from './assets/Start_Gate.png'
+import DRRgate2 from './assets/End_Gate.png'
+import { useState } from 'react'
 
 function App() {
 
+  let [square, setSquare] = useState(false);
+
   let goTo = () => {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ','_blank');
-    // window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  }
+
+  let mouseOver = () => {
+    setSquare(square = true);
+  }
+
+  let mouseOut = () => {
+    setSquare(square = false);
   }
 
   return (
     <>
       <div className="mainImage">
         <img src={DRRbg} alt="drrBG" />
-        <img onClick={goTo} className='gate trans' src={DRRgate} alt="drrBG" />
+        <p onClick={goTo} onMouseOver={mouseOver} onMouseOut={mouseOut} className='gate square'></p>
+        {square ? <img className='gate trans second' src={DRRgate2} alt="drrBG" /> : <img className='gate trans' src={DRRgate} alt="drrBG" />}
       </div>
     </>
   )
