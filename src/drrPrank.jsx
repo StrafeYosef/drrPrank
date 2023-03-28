2ca0ba8556bd49234fdce108580f7d3a9b81fa0e
Yosef <yosefplayz4580@gmail.com>
2023-03-28 22:43:36 +0300

    added some rickroll

--- src/drrPrank.jsx
+++ src/drrPrank.jsx
@@ -1,15 +1,16 @@
 import DRRbg from './assets/homePage.png'
 import DRRgate from './assets/Start_Gate.png'
 import DRRgate2 from './assets/End_Gate.png'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import rickRoll from './assets/rickRoll.gif'
 
 function App() {
 
   let [square, setSquare] = useState(false);
+  let [show, setShow] = useState(false);
 
-  let goTo = () => {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ','_blank');
-  }
+  // let strings = ['this is the first', 'this is second', 'this is third'];
+  // let [string, setString] = useState('this is the first');
 
   let mouseOver = () => {
     setSquare(square = true);
@@ -19,11 +20,44 @@ function App() {
     setSquare(square = false);
   }
 
+  let activateIt = () => {
+    setTimeout(() => {
+      setShow(true);
+    }, 750);
+  }
+
+  // let i = 0;
+
+  // function eachOne() {
+  //   if (i <= strings.length) {
+  //     i++;
+  //     console.log(strings[i - 1])
+  //   }
+  //   if (i == strings.length) {
+  //     i = 0;
+  //   }
+  //  setTimeout(eachOne, 3000);
+  //  setString(strings[i])
+  // }
+
+  // useEffect(() => {
+  //   eachOne();
+  // }, [])
+
+
+  // let goEach = () => {
+  // }
+
   return (
     <>
       <div className="mainImage">
         <img src={DRRbg} alt="drrBG" />
-        <p onClick={goTo} onMouseOver={mouseOver} onMouseOut={mouseOut} className='gate square'></p>
+        <p onClick={activateIt} onMouseOver={mouseOver} onMouseOut={mouseOut} className='gate square'></p>
+        {/* <p className='text'>{string}</p> */}
+        {show && <div className="dialogConatiner">
+            <img src={rickRoll} alt="rickRoll" />
+            <p>We are still working on the Alpha version.<br/>Help us be supporting on patreon:<br/> <a href="https://www.patreon.com/DungeonRampageRemakeDRR" target="_blank">https://www.patreon.com/DungeonRampageRemakeDRR</a><br/>April Fools by DRR team.</p>
+        </div> }
         {square ? <img className='gate trans second' src={DRRgate2} alt="drrBG" /> : <img className='gate trans' src={DRRgate} alt="drrBG" />}
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './drrPrank'
 import './main.css'
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAAHqjy3x-lqLUZXsIaQh9UxF9NgsdJN_U",
+  
+    authDomain: "drralpha-d060d.firebaseapp.com",
+  
+    projectId: "drralpha-d060d",
+  
+    storageBucket: "drralpha-d060d.appspot.com",
+  
+    messagingSenderId: "342621170320",
+  
+    appId: "1:342621170320:web:3b97b0e4cb63afa29bc615"
+  
+  };
+  
+  const app = initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('drr')).render(
     <App />
 )
